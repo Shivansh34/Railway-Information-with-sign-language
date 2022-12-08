8d6997458164fc0f5bbe880479180df625352870
architchopra <57140059+architchopra@users.noreply.github.com>
2022-12-08 17:54:15 +0530

    pnr

--- frontend/src/classifier/index.jsx
+++ frontend/src/classifier/index.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6530b3288e3faddc9e9bebe6cd9ed6ed10e438b71ab093b3f1aefd19d0b846c0
-size 6730
+oid sha256:4b84e26c32f5aba6697335e36cecec7bc3f23b41d5207f2d202b16cfe0336f5c
+size 6975
