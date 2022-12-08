c77a8a146bd178a80d9ef7356bb1b278d9941815
shivanshgupta340 <shivanshgupta340@gmail.com>
2022-12-08 17:52:00 +0530

    remove ee

--- frontend/src/classifier/index.jsx
+++ frontend/src/classifier/index.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ebaf12a236236231607abe007fe08e77be4b6028a826c477a1f7c46f2c7c9156
-size 3413
+oid sha256:328aa6ec52c45506765818d478ed3ea55c5b9fa06d4940cda2fcded181437809
+size 3214
