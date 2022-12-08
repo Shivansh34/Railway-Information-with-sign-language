623797fecabc32a79aa7166d227c2201abf57150
shivanshgupta340 <shivanshgupta340@gmail.com>
2022-12-08 19:06:44 +0530

    mode

--- frontend/src/classifier/index.jsx
+++ frontend/src/classifier/index.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8515c071ed0ba02487cb4626d17ab140a196c2cca21d1d78be8acef479ee17e3
-size 7212
+oid sha256:28222f6a4b4fc8ebb49664989951f6ce06523dec07f092a63402ddcca403bc1f
+size 7939
